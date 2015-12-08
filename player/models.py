86ff017e0e4f31916ece3afecc91acfa31847ad2
paconte <paconte@gmail.com>
2015-12-08 18:36:01 +0100

    Make models.py python 3 conform and make it more PEP conform.

--- player/models.py
+++ player/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
 from django.core.validators import MinValueValidator, MaxValueValidator
-#from geography.models import ZipCode
+
+DATA_FILES = './data_files/'
 
 MIXED_OPEN = 'Mixed Open'
 MENS_OPEN = 'Mens Open'
@@ -16,7 +16,8 @@ TOUCH_DIVISION_CHOICES = (
     ('SMX', SENIOR_MIX),
     ('SMO', SENIOR_MENS),
     ('SWO', SENIOR_WOMENS),
-    )
+)
+
 
 # Create your models here.
 class Person(models.Model):
@@ -25,49 +26,52 @@ class Person(models.Model):
     GENDER_CHOICES = (
         (MALE, 'Male'),
         (FEMALE, 'Female'),
-        )
+    )
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     born = models.DateField(null=True, blank=True)
     nationality = models.CharField(max_length=30, null=True, blank=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default = MALE)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=MALE)
 
-    def __unicode__(self):  # Python 3: def __str__(self):
-        return '%s %s - %s' % (self.first_name, self.last_name, self.gender)
+    def __str__(self):
+        return '{:s} {:s} - {:s}'.format(self.first_name, self.last_name, self.gender)
 
     def _get_full_name(self):
-        "Returns the person's full name."
-        return '%s %s' % (self.first_name, self.last_name)
+        """Returns the person's full name."""
+        return '{:s} {:s}'.format(self.first_name, self.last_name)
+
 
 class Team(models.Model):
     name = models.CharField(max_length=30)
     players = models.ManyToManyField(Person, through='Player')
     division = models.CharField(max_length=3, choices=TOUCH_DIVISION_CHOICES)
 
-    def __unicode__(self):  # Python 3: def __str__(self):
-        return self.name
+    def __str__(self):
+        return self.division + ' - ' + self.name
+
 
 class Tournament(models.Model):
     name = models.CharField(max_length=50)
     country = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
-#   zip_code = models.ForeignKey(ZipCode)
+    #   zip_code = models.ForeignKey(ZipCode)
     address = models.CharField(max_length=100, null=True, blank=True)
     date = models.DateField(null=True, blank=True)
     teams = models.ManyToManyField(Team)
     division = models.CharField(max_length=3, choices=TOUCH_DIVISION_CHOICES)
 
-    def __unicode__(self):  # Python 3: def __str__(self):
-        if (self.country and self.city):
-            result = '%s - %s (%s, %s)' % (self.division, self.name, self.city, self.country)
-        elif (self.country):
-            result = '%s - %s (%s)' % (self.division, self.name, self.country)
-        elif (self.city):
-            result = '%s - %s (%s)' % (self.division, self.name, self.city)
+    def __str__(self):
+        if self.country and self.city:
+            result = '{:s} - {:s} ({:s}, {:s})'.format(self.division, self.name, self.city, self.country)
+        elif self.country:
+            result = '{:s} - {:s} ({:s})'.format(self.division, self.name, self.country)
+        elif self.city:
+            result = '{:s} - {:s} ({:s})'.format(self.division, self.name, self.city)
         else:
-            result =  '%s - %s' % (self.division, self.name)
+            result = '{:s} - {:s}'.format(self.division, self.name)
         return result
+
 
 class Player(models.Model):
     person = models.ForeignKey(Person)
@@ -75,8 +79,9 @@ class Player(models.Model):
     number = models.PositiveIntegerField(null=True, blank=True)
     tournaments_played = models.ManyToManyField(Tournament, null=True, blank=True)
 
-    def __unicode__(self):  # Python 3: def __str__(self):
-        return '%s  %s %s' % (self.team, self.number, self.person)
+    def __str__(self):
+        return '{:s},  {:s} {:s}'.format(str(self.team), str(self.number), str(self.person))
+
 
 class GameRound(models.Model):
     FINAL = 'Final'
@@ -89,7 +94,7 @@ class GameRound(models.Model):
     SIXTH_POSITION = 'Sixth position'
     SEVENTH_POSITION = 'Seventh position'
     EIGHTH_POSITION = 'Eighth position'
-    NINTH_POSITION = 'Ninth position'    
+    NINTH_POSITION = 'Ninth position'
     TENTH_POSITION = 'Tenth position'
     ELEVENTH_POSITION = 'Eleventh position'
     TWELFTH_POSITION = 'Twelfth position'
@@ -127,19 +132,19 @@ class GameRound(models.Model):
         (TENTH_POSITION, TENTH_POSITION),
         (ELEVENTH_POSITION, ELEVENTH_POSITION),
         (TWELFTH_POSITION, TWELFTH_POSITION),
-        (THIRTEENTH_POSITION,THIRTEENTH_POSITION),
-        (FIFTEENTH_POSITION,FIFTEENTH_POSITION),
-        (SIXTEENTH_POSITION,SIXTEENTH_POSITION),
-        (EIGHTEENTH_POSITION,EIGHTEENTH_POSITION),
-        (TWENTIETH_POSITION,TWENTIETH_POSITION),        
+        (THIRTEENTH_POSITION, THIRTEENTH_POSITION),
+        (FIFTEENTH_POSITION, FIFTEENTH_POSITION),
+        (SIXTEENTH_POSITION, SIXTEENTH_POSITION),
+        (EIGHTEENTH_POSITION, EIGHTEENTH_POSITION),
+        (TWENTIETH_POSITION, TWENTIETH_POSITION),
         (DIVISION, DIVISION),
         (POOL_A, POOL_A),
         (POOL_B, POOL_B),
         (POOL_C, POOL_C),
         (POOL_D, POOL_D),
         (LIGA, LIGA),
-        )
-    
+    )
+
     GOLD = 'Gold'
     SILVER = 'Silver'
     BRONZE = 'Bronze'
@@ -150,69 +155,118 @@ class GameRound(models.Model):
         (SILVER, SILVER),
         (BRONZE, BRONZE),
         (WOOD, WOOD),
-        )
-    
+    )
+
     round = models.CharField(default=POOL_A, max_length=20, null=False, blank=False, choices=GAME_ROUND_CHOICES)
     number_teams = models.PositiveIntegerField(default=2, validators=[MinValueValidator(0), MaxValueValidator(20)])
     category = models.CharField(default=GOLD, max_length=6, null=False, blank=False, choices=CATEGORY_ROUND_CHOICES)
 
-    def __unicode__(self):  # Python 3: def __str__(self):
-        return '%s %s %s' % (self.round, self.number_teams, self.category)
+    def __str__(self):
+        return '{:s} {:d} {:s}'.format(self.round, self.number_teams, self.category)
 
     def is_pool(self):
         return self.round == self.POOL_A or self.round == self.POOL_B or self.round == self.POOL_C or self.round == self.POOL_D
 
+    def __lt__(self, other):
+        #        print('self = %s, other = %s' %(self, other))
+        if self.category == other.category:
+            if self.round == other.round:
+                result = self.number_teams.__lt__(other.number_teams)
+            else:
+                if self.round == self.FINAL:
+                    result = False
+                elif other.round == self.FINAL:
+                    result = True
+                elif self.round == self.THIRD_POSITION:
+                    result = False
+                elif other.round == self.THIRD_POSITION:
+                    result = True
+                elif self.round == self.SEMI:
+                    result = False
+                elif other.round == self.SEMI:
+                    result = True
+                elif self.round == self.QUARTER:
+                    result = False
+                elif other.round == self.QUARTER:
+                    result = True
+                elif self.round == self.SIXTEENTH:
+                    result = False
+                elif other.round == self.SIXTEENTH:
+                    result = True
+                else:
+                    raise Exception('Problem comparing values: %s and  %s' % (self.round, other.round))
+        else:
+            if self.category == self.GOLD:
+                result = False
+            elif other.category == self.GOLD:
+                result = True
+            elif self.category == self.SILVER:
+                result = False
+            elif other.category == self.SILVER:
+                result = True
+            elif self.category == self.BRONZE:
+                result = False
+            elif other.category == self.BRONZE:
+                result = True
+            elif self.category == self.WOOD:
+                result = False
+            else:
+                raise Exception('Problem comparing values: %s and  %s' % (self.category, other.category))
+        return result
+
     def __cmp__(self, other):
-#        print('self = %s, other = %s' %(self, other))
-        if (self.category == other.category):
-            if (self.round == other.round):
+        #        print('self = %s, other = %s' %(self, other))
+        if self.category == other.category:
+            if self.round == other.round:
                 result = self.number_teams.__cmp__(other.number_teams)
             else:
-                if (self.round == self.FINAL):
+                if self.round == self.FINAL:
                     result = 1
-                elif (other.round == self.FINAL):
+                elif other.round == self.FINAL:
                     result = -1
-                elif (self.round == self.THIRD_POSITION):
+                elif self.round == self.THIRD_POSITION:
                     result = 1
-                elif (other.round == self.THIRD_POSITION):
+                elif other.round == self.THIRD_POSITION:
                     result = -1
-                elif (self.round == self.SEMI):
+                elif self.round == self.SEMI:
                     result = 1
-                elif (other.round == self.SEMI):
+                elif other.round == self.SEMI:
                     result = -1
-                elif (self.round == self.QUARTER):
+                elif self.round == self.QUARTER:
                     result = 1
-                elif (other.round == self.QUARTER):
+                elif other.round == self.QUARTER:
                     result = -1
-                elif (self.round == self.SIXTEENTH):
+                elif self.round == self.SIXTEENTH:
                     result = 1
-                elif (other.round == self.SIXTEENTH):
+                elif other.round == self.SIXTEENTH:
                     result = -1
                 else:
                     raise Exception('Problem comparing values: %s and  %s' % (self.round, other.round))
         else:
-            if (self.category == self.GOLD):
+            if self.category == self.GOLD:
                 result = 1
-            elif (other.category == self.GOLD):
+            elif other.category == self.GOLD:
                 result = -1
-            elif (self.category == self.SILVER):
+            elif self.category == self.SILVER:
                 result = 1
-            elif (other.category == self.SILVER):
+            elif other.category == self.SILVER:
                 result = -1
-            elif (self.category == self.BRONZE):
+            elif self.category == self.BRONZE:
                 result = 1
-            elif (other.category == self.BRONZE):
+            elif other.category == self.BRONZE:
                 result = -1
-            elif (self.category == self.WOOD):
+            elif self.category == self.WOOD:
                 result = 1
-            else: 
+            else:
                 raise Exception('Problem comparing values: %s and  %s' % (self.category, other.category))
         return result
 
+
 class GameField(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    def __unicode__(self):  # Python 3: def __str__(self):
-        return '%s' % (self.name)
+
+    def __str__(self):  # Python 3: def __str__(self):
+        return '{}'.format(self.name)
 
 
 class Game(models.Model):
@@ -222,15 +276,20 @@ class Game(models.Model):
     visitor = models.ForeignKey(Team, related_name="visitor", null=True, blank=True)
     local_score = models.SmallIntegerField(null=True, blank=True)
     visitor_score = models.SmallIntegerField(null=True, blank=True)
-    #player_stadistics = models.ManyToManyField(Stadictic)
+    # player_stadistics = models.ManyToManyField(Stadictic)
     tournament = models.ForeignKey(Tournament)
     phase = models.ForeignKey(GameRound)
 
-    def __unicode__(self):  # Python 3: def __str__(self):
-        return '%s - %s - %s %s - %s %s' % (self.tournament, self.phase, self.local, self.local_score, self.visitor_score, self.visitor)
+    def __str__(self):
+        return '{} - {} - {} {} - {} {}'.format(
+            self.tournament, self.phase, self.local, self.local_score, self.visitor_score, self.visitor)
+
+    def __lt__(self, other):
+        return self.phase.__lt__(other.phase)
 
     def __cmp__(self, other):
         return self.phase.__cmp__(other.phase)
+
 
 class PlayerStadistic(models.Model):
     game = models.ForeignKey(Game)
@@ -238,12 +297,12 @@ class PlayerStadistic(models.Model):
     points = models.PositiveIntegerField(null=True, blank=True, default=0)
     assistances = models.PositiveIntegerField(null=True, blank=True, default=0)
 
-    def __unicode__(self):  # Python 3: def __str__(self):
-        return '%s - %s - touchdowns: %s' % (self.game, self.player, self.points)
+    def __str__(self):
+        return '{} - {} - touchdowns: {}'.format(self.game, self.player, self.points)
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=40, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
     where = models.CharField(max_length=20, null=True, blank=True)
     message = models.TextField(null=False, blank=False)
-
