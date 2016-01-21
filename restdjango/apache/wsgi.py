f0ff75ff81e13d54fa5f930d7f968cc590145519
paconte <paconte@gmail.com>
2016-01-21 16:16:12 +0000

    Add path of app to the wsgi file.

--- restdjango/apache/wsgi.py
+++ restdjango/apache/wsgi.py
@@ -7,8 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restdjango.apache.settings_apache")
+import os, sys
 
+apache_configuration= os.path.dirname(__file__)
+project = os.path.dirname(apache_configuration)
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+sys.path.append(project)
+sys.path.append('/home/django')
+sys.path.append('/home/django/django-tournament.git/restdjango')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restdjango.apache.settings_apache")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
