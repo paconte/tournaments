bb3199c233414e5732dcf620dc42eda0be14f3e6
paconte <paconte@gmail.com>
2016-01-21 16:14:09 +0000

    Add the aplication urls at the root of the ip address where django is running according to apache configuration.

--- restdjango/urls.py
+++ restdjango/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+    url(r'', include('player.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tournaments/', include('player.urls')),
     # Examples:
     # url(r'^$', 'restdjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^tournaments/', include('player.urls')),
 )
