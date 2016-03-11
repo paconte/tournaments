f5b30d17077ef45535ab7503028b7a4174b0fe03
paconte <paconte@gmail.com>
2016-03-11 17:58:31 +0100

    Add test for parsing and saving in DB Fox tournaments MO, WO, MXO and W27.

--- player/tests.py
+++ player/tests.py
@@ -1,7 +1,7 @@
-import unittest
 import os.path
 
 from django.test import TestCase
+from django.test import TransactionTestCase
 
 from player import csvWriter
 from player import csvdata
@@ -10,40 +10,60 @@ from player import csvReader
 
 # Create your tests here.
 class CsvFileTest(TestCase):
+    # fixtures = ['player.GameRound.json']
+
     def tearDown(self):
-        pass
+        directory = csvdata.RAW_STATS_FILES
+        if os.path.exists(directory) and os.path.isdir(directory):
+            for filename in os.listdir(directory):
+                if filename.endswith('.test'):
+                    os.remove(filename)
 
-    #def setUp(self):
-    #    reader = csvReader.CsvReader(csvReader.CsvReader.PHASE)
-    #    reader.read_file('./player/data_files/csv/TPhases.csv', csvdata.CsvPhase)
-
-    def test_csv_fox_tournament(self):
-        tournament = csvWriter.FoxGamesManager.WC_2015_WO_FOX
-        self.csv_fox_tournament(tournament)
-
-    def csv_fox_tournament(self, tournament):
-        fox_manager = csvWriter.FoxGamesManager(tournament)
-        fox_manager.extract_raw_games()
-        fox_manager.convert_fox_games_to_csv_games()
-        #fox_manager.download_games_statistics()
-        fox_manager.extract_statistics()
-
-        writer = csvWriter.CsvWriter(tournament, False)
-        writer.delete_path()
-        writer.write_csv(fox_manager.get_csv_games())
-        self.assertTrue(os.path.isfile(writer.get_path()))
-
+    def setUp(self):
         reader = csvReader.CsvReader(csvReader.CsvReader.PHASE)
         reader.read_file('./player/data_files/csv/TPhases.csv', csvdata.CsvPhase)
 
+    def test_fox_tournament_WC_2015_WO_FOX(self):
+        tournament = csvWriter.FoxGamesManager.WC_2015_WO_FOX
+        self.csv_fox_tournament(tournament)
+
+    def test_fox_tournament_WC_2015_MO_FOX(self):
+        tournament = csvWriter.FoxGamesManager.WC_2015_MO_FOX
+        self.csv_fox_tournament(tournament)
+
+    def test_fox_tournament_WC_2015_MXO_FOX(self):
+        tournament = csvWriter.FoxGamesManager.WC_2015_MXO_FOX
+        self.csv_fox_tournament(tournament)
+
+    def test_fox_tournament_WC_2015_W27_FOX(self):
+        tournament = csvWriter.FoxGamesManager.WC_2015_W27_FOX
+        self.csv_fox_tournament(tournament, False, False)
+
+    def csv_fox_tournament(self, tournament, games_download=False, stats_download=False):
+        fox_manager = csvWriter.FoxGamesManager(tournament)
+        if games_download:
+            fox_manager.download_games_files()
+        fox_manager.extract_raw_games()
+        if stats_download:
+            fox_manager.download_games_statistics()
+        fox_manager.extract_statistics()
+
+        writer = csvWriter.CsvWriter(tournament, False)
+        writer.delete_filename_path()
+        writer.write_csv(fox_manager.get_fox_games())
+        self.assertTrue(os.path.isfile(writer.get_filename_path()))
+
         reader = csvReader.CsvReader(csvReader.CsvReader.TOURNAMENT)
-        reader.read_file(writer.get_path(), csvdata.CsvGame)
+        reader.read_file(writer.get_filename_path(), csvdata.CsvGame)
 
         writer = csvWriter.CsvWriter(tournament, True)
-        writer.delete_path()
+        writer.delete_filename_path()
         writer.write_csv(fox_manager.get_csv_statistics())
-        self.assertTrue(os.path.isfile(writer.get_path()))
+        self.assertTrue(os.path.isfile(writer.get_filename_path()))
+
+        reader = csvReader.CsvReader(csvReader.CsvReader.NTS_STADISTIC)
+        reader.read_file(writer.get_filename_path(), csvdata.CsvNTSStadistic)
 
 
-#class kkk(TestCase):
-#    pass
+        # class kkk(TestCase):
+        #    pass
