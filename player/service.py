09d01ceaaa0e3ba8a71618dabf0fbe476c1708a9
paconte <paconte@gmail.com>
2016-03-24 17:29:03 +0100

    minor change.

--- player/service.py
+++ player/service.py
@@ -380,7 +380,7 @@ def sort_tournament_list(tournament_list):
         elif 'Capital Cup' in t.name:
             result["Germany"].append(t)
         elif 'NTL' in t.name:
-            result['Australia'].append(t)
+            result["Australia"].append(t)
         else:
             raise Exception('Tournament name %s not recognized.' % t.name)
     result["England"] = sorted(result.get("England"))
