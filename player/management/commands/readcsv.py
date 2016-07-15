af928a5cf5e68d390800e453182acd4beec3250d
paconte <paconte@gmail.com>
2016-07-15 16:28:02 +0200

    Add feature: read fit stats

--- player/management/commands/readcsv.py
+++ player/management/commands/readcsv.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     help = 'Add csv data to the database.'
 
     def add_arguments(self, parser):
-        parser.add_argument('type', choices=['games', 'stats', 'phases'])
+        parser.add_argument('type', choices=['games', 'phases', 'stats_game', 'stats_tournament'])
         parser.add_argument('file_path', nargs='+')
 
     def handle(self, *args, **options):
@@ -19,9 +19,12 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS('Read csv file: "%s"' % file_path))
 
-        if csv_type == 'stats':
+        if csv_type == 'stats_game':
             reader = csvReader.CsvReader(csvReader.CsvReader.NTS_STATISTIC)
             reader.read_file(file_path, csvdata.CsvNTSStatistic)
+        elif csv_type == 'stats_tournament':
+            reader = csvReader.CsvReader(csvReader.CsvReader.FIT_STATISTIC)
+            reader.read_file(file_path, csvdata.FitStatistic)
         elif csv_type == 'games':
             reader = csvReader.CsvReader(csvReader.CsvReader.TOURNAMENT)
             reader.read_file(file_path, csvdata.CsvGame)
