b4cdf783ed8029248e9ea46861cb6a304a201672
paconte <paconte@gmail.com>
2016-08-04 11:56:37 +0200

    Set default settings to touchDB.

--- touchdb/settings.py
+++ touchdb/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
 #    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'padelDB',
+        'NAME': 'touchDB',
         'USER': 'tournamentDB',
         'PASSWORD': 'DBtournament',
         'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
