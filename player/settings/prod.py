4ca8dd0e1e19175a6e5220c023b6c0b816e78216
paconte <paconte@gmail.com>
2016-10-27 15:24:39 +0200

    Apply new settings strategy 3

--- player/settings/prod.py
+++ player/settings/prod.py
@@ -1,14 +1,78 @@
-from .dev import *
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'a%#zm8z^&=+ela1rq&0*pg2d+)!8b*q92b76e46-@04&78rx96'
+
+# DEBUG configuration
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 # HOSTS
-
 ALLOWED_HOSTS = ['62.75.188.101', 'www.rollball.net', 'rollball.net']
 
-# LOGGING configuration
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'player'
+)
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+ROOT_URLCONF = 'player.urls'
+WSGI_APPLICATION = 'player.settings.wsgi.application'
 
+
+# App Admins and Managers:
+ADMINS = (
+    ('Francisco Revilla', 'paconte@gmail.com'),
+)
+MANAGERS = (
+    ('Francisco Revilla', 'paconte@gmail.com'),
+)
+
+# Database
+DATABASES = {
+    #    'default': {
+    #        'ENGINE': 'django.db.backends.sqlite3',
+    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'touchDB',
+        'USER': 'tournamentDB',
+        'PASSWORD': 'DBtournament',
+        'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',  # Set to empty string for default.
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# LOGGING configuration
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
