ad75be1750902998815515e0a40081d683e8ada9
paconte <paconte@gmail.com>
2016-11-10 09:50:42 +0100

    Change wsgi.py

--- player/settings/wsgi.py
+++ player/settings/wsgi.py
@@ -5,11 +5,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
 
-import os, sys
 
-apache_configuration= os.path.dirname(__file__)
+import os
+import sys
+
+apache_configuration = os.path.dirname(__file__)
 project = os.path.dirname(apache_configuration)
 workspace = os.path.dirname(project)
 sys.path.append(workspace)
@@ -17,5 +18,12 @@ sys.path.append(project)
 sys.path.append('/home/django')
 sys.path.append('/home/django/django-tournament.git/player')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "player.settings.prod")
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "player.settings.prod")
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
