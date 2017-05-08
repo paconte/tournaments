b3406ca249b2e931663cd3114759de5bc623db25
paconte <paconte@gmail.com>
2017-05-08 20:06:23 +0200

    PADEL: Add new tag to access team players.

--- player/templatetags/tag_tournament.py
+++ player/templatetags/tag_tournament.py
@@ -4,3 +4,13 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def get_item_tuple_first(dictionary, key):
+    return dictionary.get(key)[0].person.get_full_name_reverse()
+
+
+@register.filter
+def get_item_tuple_second(dictionary, key):
+    return dictionary.get(key)[1].person.get_full_name_reverse()
